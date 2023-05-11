61aa6646050b4847f9d32982dd45ded8f532b85e
Jadhavrohini <133083899+Jadhavrohini@users.noreply.github.com>
2023-05-11 10:00:03 +0530

    Add files via upload
    A new branch is created

---
+++ Temperature.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+	float f,c;
+	printf("Enter temperature in fahrenheight:")
+	scanf("%f",&f);
+	c=(f-32)*(5\9);
+	printf("Temperature in celcius: %f",c);
+	
+	
+}
+
+
+
+
+
+
